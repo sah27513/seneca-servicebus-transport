6abe8bd55ad6818941817932cec00973a68a6239
Stephen Halliburton <stephen.halliburton@newwave.io>
2019-07-09 15:30:24 -0400

    Changed servicebus type

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-'use strict'
+'use strict';
 /**
  * @module seneca-servicebus-transport
  */
-const defaults = require('./defaults')
-const hooks = require('./lib/hooks')
+const defaults = require('./defaults');
+const hooks = require('./lib/hooks');
 
-const PLUGIN_NAME = 'servicebus-transport'
-const TRANSPORT_TYPE = 'servicebus'
+const PLUGIN_NAME = 'servicebus-transport';
+const TRANSPORT_TYPE = 'servicebus';
 
-const debug = require('debug')('seneca-servicebus-transport')
+const debug = require('debug')('seneca-servicebus-transport');
 
-module.exports = function (opts) {
-  var seneca = this
-  var so = seneca.options()
-  var options = seneca.util.deepextend(defaults, so.transport, opts)
+module.exports = opts => {
+  var seneca = this;
+  var so = seneca.options();
+  var options = seneca.util.deepextend(defaults, so.transport, opts);
 
-  debug('Initializing plugin with options %%O', options.servicebus)
+  debug('Initializing plugin with options %%O', options.servicebus);
+  console.log('Initializing Service Bus');
 
-  var listener = hooks.listenerHook(seneca)
-  var client = hooks.clientHook(seneca)
-  seneca.add({
-    role: 'transport',
-    hook: 'listen',
-    type: TRANSPORT_TYPE
-  }, listener.hook(options, 'listen'))
-  seneca.add({
-    role: 'transport',
-    hook: 'client',
-    type: TRANSPORT_TYPE
-  }, client.hook(options, 'client'))
+  var listener = hooks.listenerHook(seneca);
+  var client = hooks.clientHook(seneca);
+  seneca.add(
+    {
+      role: 'transport',
+      hook: 'listen',
+      type: TRANSPORT_TYPE
+    },
+    listener.hook(options, 'listen')
+  );
+  seneca.add(
+    {
+      role: 'transport',
+      hook: 'client',
+      type: TRANSPORT_TYPE
+    },
+    client.hook(options, 'client')
+  );
 
   return {
     name: PLUGIN_NAME
-  }
-}
+  };
+};
